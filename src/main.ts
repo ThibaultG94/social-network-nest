ee0ac8625dbdb836f3a2e2dbd4a03690ea955798
Thibault Guilhem <thibault.guilhem@gmail.com>
2024-10-27 10:43:48 +0100

    Ajout de la validation globale des requêtes et création du DTO CreatePostDto avec des règles de validation pour le contenu et l'ID du post parent

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(new ValidationPipe({
+      whitelist: true, // Supprime les propriétés non définies dans les DTOs
+      transform: true, // Transforme automatiquement les types
+      forbidNonWhitelisted: true, // Rejette les requêtes avec des propriétés non définies
+    })
+  )
+  await app.listen(process.env.PORT ?? 3000); 
 }
 bootstrap();
--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,1 +1,18 @@
-export class CreatePostDto {}
+import { IsOptional, IsString, IsUUID, MaxLength, MinLength } from "class-validator";
+
+export class CreatePostDto {
+    @IsString()
+    @MinLength(1, {
+        message: 'Le contenu ne peut pas être vide'
+    })
+    @MaxLength(500, {
+        message: 'Le contenu ne peut pas dépasser 500 caractères'
+    })
+    content: string;
+
+    @IsUUID('4', {
+        message: 'L\'ID du post parent doit être un UUID valide'
+    })
+    @IsOptional()
+    parentPostId?: string;
+}
