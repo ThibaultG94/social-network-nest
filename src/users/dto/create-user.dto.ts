fed258ec0ce75e83a4f2bec7cf323e0c8578bc2f
Thibault Guilhem <thibault.guilhem@gmail.com>
2024-10-27 10:24:22 +0100

    Ajout de la validation des données dans CreateUserDto pour le nom d'utilisateur, l'email, le mot de passe, la bio et la date de naissance

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,35 @@
-export class CreateUserDto {}
+import { IsDate, IsEmail, IsOptional, IsString, Matches, MaxLength, MinLength } from "class-validator";
+
+export class CreateUserDto {
+    @IsString()
+    @MinLength(3)
+    @MaxLength(30)
+    @Matches(/^[a-zA-Z0-9_-]*$/, {
+        message: 'Le nom d\'utilisateur ne peut contenir que des lettres, chiffres, underscores et tirets'
+    })
+    username: string;
+
+    @IsEmail()
+    @MaxLength(254)
+    @Matches(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, {
+        message: 'Adresse email invalide'
+    })
+    email: string;
+
+    @IsString()
+    @MinLength(8)
+    @MaxLength(80)
+    @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])/, {
+        message: 'Le mot de passe doit contenir au moins une lettre minuscule, une lettre majuscule, un chiffre et un caractère spécial'
+    })
+    password: string;
+
+    @IsString()
+    @IsOptional()
+    @MaxLength(160)
+    bio?: string;
+
+    @IsDate()
+    @IsOptional()
+    dateOfBirth?: Date;
+}
