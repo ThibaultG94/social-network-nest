85f6a8fa30da99bd9bb990fade77ce608e08de79
Thibault Guilhem <thibault.guilhem@gmail.com>
2024-11-13 13:27:19 +0100

    Ajout du module Follows avec intégration de TypeOrm pour la gestion des entités Follow et dépendance au module Users pour la vérification des utilisateurs.

--- src/follows/follows.module.ts
+++ src/follows/follows.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { FollowsService } from './follows.service';
 import { FollowsController } from './follows.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Follow } from './entities/follow.entity';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Follow]),
+    UsersModule // Nécessaire pour vérifier l'existence des utilisateurs
+  ],
   controllers: [FollowsController],
   providers: [FollowsService],
+  exports: [FollowsService] // Permet l'utilisation du service dans d'autres modules
 })
 export class FollowsModule {}
