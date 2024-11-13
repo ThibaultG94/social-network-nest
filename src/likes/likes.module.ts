80e869cc36d1001b4d87650c717c8942e5c5d3f8
Thibault Guilhem <thibault.guilhem@gmail.com>
2024-11-13 15:18:11 +0100

    Ajout du module Likes avec intégration de TypeOrm pour la gestion des entités Like, et dépendances aux modules Posts et Users pour la vérification des posts et des utilisateurs. Exportation du service Likes pour utilisation dans d'autres modules.

--- src/likes/likes.module.ts
+++ src/likes/likes.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { LikesService } from './likes.service';
 import { LikesController } from './likes.controller';
+import { Like } from './entities/like.entity';
+import { PostsModule } from '../posts/posts.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Like]),
+    PostsModule, // Pour vérifier l'existence des posts
+    UsersModule, // Pour vérifier l'existence des utilisateurs
+  ],
   controllers: [LikesController],
   providers: [LikesService],
+  exports: [LikesService], // Pour pouvoir utiliser le service dans d'autres modules
 })
 export class LikesModule {}
